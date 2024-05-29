26834bad33b2ea2a39dcfb213ea6b96870275cf3
git-show <148685197+git-show@users.noreply.github.com>
2024-05-29 11:10:19 +0900

    Update BootBroadcastReceiver.kt

--- android/src/main/kotlin/yukams/app/background_locator_2/BootBroadcastReceiver.kt
+++ android/src/main/kotlin/yukams/app/background_locator_2/BootBroadcastReceiver.kt
@@ -1,27 +1,13 @@
 package yukams.app.background_locator_2
 
 import android.content.BroadcastReceiver
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.util.Log
 
-
-class RebootBroadcastReceiver : BroadcastReceiver() {
+class BootBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        Log.d(
-            "RebootBroadcastReceiver", 
-            "Received broadcast: ${intent?.action ?: "null action"}"
-        )
-        when (intent.action) {
-            Intent.ACTION_REBOOT, Intent.ACTION_BOOT_COMPLETED -> {
-                Log.i("NotificationListener", "Registering notification listener, after reboot!")
-                BackgroundLocatorPlugin.registerAfterBoot(context)
-            }
-            else -> {
-                Log.i("NotificationListener", intent.action.toString())
-            }
+        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+            BackgroundLocatorPlugin.registerAfterBoot(context)
         }
     }
 }
